c2236c1532acf51a364d227b833798e047da793e
lala3069 <lala3069@colorado.edu>
2024-12-09 03:15:31 -0700

    Update package for pip install

---
+++ InSAR_Calculation/__init__.py
@@ -0,0 +1,1 @@
+from InSAR_calculation import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='InSAR_Calculation',
+      version='0.1',
+      description='',
+      url='https://github.com/lala3069/InSAR_Calculation.git',
+      author='Sang Lapinee',
+      author_email='lala3069@colorado.edu',
+      license='MIT',
+      packages=['InSAR_Calculation'],
+      zip_safe=False)
